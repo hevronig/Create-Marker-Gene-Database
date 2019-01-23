5d5788a3b4a7f468c8be4ba1b6919cccb4027f31
Gur Hevroni <hevronig@gmail.com>
2019-01-23 15:37:19 +0200

    run updated script (cd-hit) to generate output files

--- generate_annotation.sh
+++ generate_annotation.sh
@@ -5,7 +5,7 @@ source $HOME/.zshrc
 ## Process FASTA headers and md5 hash ids to create complete annotation file
 
 FASTA=$1
-#ENTRYNAME="${FASTA%.*}.entry_name.tsv"
+ENTRYNAME="${FASTA%.*}.entry_name.tsv"
 TAB="${FASTA%.*}.tab"
 ANNOTATION="${FASTA%.*}.annotation.tsv"
 MD5HEADER="${FASTA%.*}.derep.md5_header.tsv"
@@ -13,11 +13,18 @@ DEREP="${FASTA%.*}.derep.fa"
 
 echo Dereplicating FASTA and assigning md5 hash-ids
 
+#vsearch --derep_fulllength $FASTA --fasta_width 0 --relabel_keep --relabel_md5 --minseqlength 0 --maxseqlength 50000000 --output $DEREP
+
 ## Dereplicate sequences by 97 percent similarity
 cd-hit -i $FASTA -o $DEREP -c 0.97
+
 ## Convert FASTA to one-line format and remove header description (after whitespace)
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < $DEREP | awk '{ print $1 }' > $DEREP.tmp
 mv $DEREP.tmp $DEREP
+
+echo Create a header:entry_name index from $FASTA
+grep '>' $DEREP | cut -d'>' -f2 | awk '{ split($0, a, " "); printf "%s\t", a[1]; split(a[1], b, "|"); printf "%s\n", b[3] }' > $ENTRYNAME
+
 ## Convert headers to md5
 python3 convert_fasta_headers_md5.py $DEREP $MD5HEADER
 
